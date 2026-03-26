345bcdddb2bde3cd9d989224128b8530ed37528d
Arun <arunravichandran1905@gmail.com>
2026-03-26 12:22:54 +0530

    Day 2 Add request Handling and Service Layer

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.services.processor import process_query
 
 app=FastAPI()
 
 @app.get("/health")
 def health():
-    return {"status": "Good"}
+    return {"status":"Ok"}
+
+
+class Queryvalidation(BaseModel):
+    question:str
 
 @app.post("/query")
-def query():
-    return {"response": "Query you send will be get processed and return to the server."}
+def query(request:Queryvalidation):
+    question=request.question
+    formatted_question=process_query(question)
+    return {"response":formatted_question}
+
---
+++ app/services/processor.py
@@ -0,0 +1,4 @@
+#Separate Logic for Scaling Later
+
+def process_query(user_query):
+    return f"Question that you asked, {user_query}"
